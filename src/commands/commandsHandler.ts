f69e65c9d0413a3a9d5bf29424251657778403ff
Dokthoror <pascalguyguy@gmail.com>
2020-11-25 08:39:22 +0100

    [new] add botinfo command

--- src/commands/commandsHandler.ts
+++ src/commands/commandsHandler.ts
@@ -3,7 +3,8 @@ import { pingCommand } from './ping';
 import { helpCommand } from './help';
 import { kickCommand } from './kick';
 import { banCommand } from './ban'
+import { botinfoCommand } from './botinfo';
 
 
 // Export commands handler, which is an array of Command objects
-export const commandsHandler = [pingCommand, helpCommand, kickCommand, banCommand];
+export const commandsHandler = [pingCommand, helpCommand, kickCommand, banCommand, botinfoCommand];
