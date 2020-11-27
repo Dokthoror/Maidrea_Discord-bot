0f6c141a90c6eea3d512763ad06554ca10be987f
Dokthoror <pascalguyguy@gmail.com>
2020-11-27 22:51:41 +0100

    Update commandHandler

--- src/commands/commandsHandler.ts
+++ src/commands/commandsHandler.ts
@@ -4,7 +4,9 @@ import { helpCommand } from './help';
 import { kickCommand } from './kick';
 import { banCommand } from './ban'
 import { botinfoCommand } from './botinfo';
+import { baguetteCommand } from './baguette';
+import { awooifyCommand } from './awooify';
 
 
 // Export commands handler, which is an array of Command objects
-export const commandsHandler = [pingCommand, helpCommand, kickCommand, banCommand, botinfoCommand];
+export const commandsHandler = [pingCommand, helpCommand, kickCommand, banCommand, botinfoCommand, baguetteCommand, awooifyCommand];
