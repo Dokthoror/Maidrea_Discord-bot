c23b929f044a942edcee39ac17e69c2632d0aa05
nerdthatnoonelikes <kenrag24@gmail.com>
2020-11-27 15:34:28 -0600

    added some image manipulation commands

---
+++ src/commands/baguette/index.ts
@@ -0,0 +1,33 @@
+// Import dependencies
+import { DiscordAPIError, Message, PermissionResolvable, MessageEmbed, GuildMember } from 'discord.js';
+import { Command } from '../../modules/Command';
+import { Embed } from '../../modules/Embed';
+import { ErrorEmbed } from '../../modules/ErrorEmbed';
+import fetch from "node-fetch"
+
+const name = "baguetter";
+
+const description = "baguettify someones avatar";
+
+const howto = "<@member: required>"
+
+const type = "FUN";
+
+const permission: PermissionResolvable = "SEND_MESSAGES";
+
+const run = async (msg: Message, args: Array<string>) => {
+    const person = msg.mentions.users.first() || msg.author;
+    const message = await msg.channel.send("Generating...");
+
+     fetch(`https://nekobot.xyz/api/imagegen?type=baguette&url=${person.displayAvatarURL({dynamic: true, size: 1024})}`)
+        .then((res) => res.json())
+        .then((body) => {
+            console.log(body)
+            let embed = new MessageEmbed()
+            .setImage(body.message)
+            .setColor("#9cc4e4")
+            message.edit(embed)
+        })
+}
+
+export const baguetteCommand = new Command(name, description, type, howto, permission, run);
