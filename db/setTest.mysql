896fb069a0f79bdf4a7b1b717433b8963d06287d
Dokthoror <pascalguyguy@gmail.com>
2020-11-30 12:43:27 +0100

    Initiate and test prisma

---
+++ db/setTest.mysql
@@ -0,0 +1,7 @@
+CREATE DATABASE maidrea_bot_db;
+USE maidrea_bot_db;
+
+CREATE TABLE prisma_test (test BOOLEAN);
+
+INSERT INTO prisma_test VALUES (true);
+INSERT INTO prisma_test VALUES (false);
--- src/events/ready/index.ts
+++ src/events/ready/index.ts
@@ -1,5 +1,9 @@
 // Import dependancies
 import { bot } from '../../../index';
+import { PrismaClient, prisma_test } from '@prisma/client';
+
+
+const prisma = new PrismaClient();
 
 
 // Export 'ready' event, when the bot logs in
@@ -7,5 +11,9 @@ export const readyEvent = {
     name: 'ready',
     run: () => {
         console.log(`Logged in as ${bot.user!.username}`);
+
+        prisma.prisma_test.findMany().then((res: prisma_test[]) => {
+            console.log(res);
+        });
     }
 }
