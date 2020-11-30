942e6d17dea467c65b39fa0f20b7fd4ea4598146
Dokthoror <pascalguyguy@gmail.com>
2020-11-30 16:16:10 +0100

    Set prisma config in main index.ts file

--- src/events/ready/index.ts
+++ src/events/ready/index.ts
@@ -1,9 +1,5 @@
 // Import dependancies
 import { bot } from '../../../index';
-import { PrismaClient, prisma_test } from '@prisma/client';
-
-
-const prisma = new PrismaClient();
 
 
 // Export 'ready' event, when the bot logs in
@@ -11,9 +7,5 @@ export const readyEvent = {
     name: 'ready',
     run: () => {
         console.log(`Logged in as ${bot.user!.username}`);
-
-        prisma.prisma_test.findMany().then((res: prisma_test[]) => {
-            console.log(res);
-        });
     }
 }
