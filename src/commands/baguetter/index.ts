8c132aaebc463a1d44949081910ce29ee7889d79
Dokthoror <pascalguyguy@gmail.com>
2020-12-31 12:01:55 +0100

    Improve syntax

--- src/commands/baguetter/index.ts
+++ src/commands/baguetter/index.ts
@@ -2,11 +2,12 @@
 import { Message, PermissionResolvable } from 'discord.js';
 import fetch from 'node-fetch';
 import Command from '../../modules/Command';
+import ErrorEmbed from '../../modules/ErrorEmbed';
 import Embed from '../../modules/Embed';
 
 const name = 'baguetter';
 
-const description = 'baguettify someones avatar';
+const description = 'baguettify someone\'s avatar';
 
 const howto = '<@member: optionnal>';
 
@@ -18,15 +19,20 @@ const run = async (msg: Message, args: Array<string>) => {
 	const person = msg.mentions.users.first() || msg.author;
 	const message = await msg.channel.send('Generating...');
 
-	fetch(`https://nekobot.xyz/api/imagegen?type=baguette&url=${person.displayAvatarURL({ dynamic: true, size: 1024 })}`)
-		.then((res) => res.json())
-		.then((body) => {
-			// console.log(body)
-			const embed = new Embed(msg.member!.displayColor)
-				.setAuthor(`${person.username} has been baguettified !`)
-				.setImage(body.message);
-			message.edit(embed);
+	try {
+		const res = await (await fetch(`https://nekobot.xyz/api/imagegen?type=baguette&url=${person.displayAvatarURL({ dynamic: true, size: 1024 })}`)).json();
+		const embed = new Embed(msg.guild!.members.cache.get(person.id)!.displayColor)
+			.setAuthor(`${person.username} has been baguettified !`)
+			.setImage(res.message);
+		message.edit({
+			content: '',
+			embed,
 		});
+	} catch (e) {
+		const embed = new ErrorEmbed()
+			.setDescription(`Unable to generate the picture: \`\`${e.message}\`\``);
+		msg.channel.send(embed);
+	}
 };
 
 const baguetteCommand = new Command(name, description, type, howto, permission, run);
